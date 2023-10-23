dd958f674c6e1eabe3c2d85df56ee11e552deb98
tvbegovic <tvrtko@adamant.hr>
2023-10-23 11:00:54 +0200

    tuple, opcioni param.

---
+++ OtplataKredita_Tuple2/Program.cs
@@ -0,0 +1,64 @@
+﻿(double anuitet, double iznosKamata) IzracunParametara(double iznos, double stopa, int brojGodina)
+{
+    if (iznos < 0 || stopa < 0 || brojGodina < 0)
+        throw new ArgumentException("Parametri moraju biti pozitivni brojevi");
+    if (stopa < 2 || stopa > 15)
+        throw new ArgumentOutOfRangeException("Stopa mora biti između 2 i 15");
+    if (brojGodina < 3 || brojGodina > 30)
+        throw new ArgumentOutOfRangeException("Broj godina mora biti 3-30");
+    var r = Kamatnjak(stopa);
+    var n = brojGodina * 12;
+    var anuitet = iznos * Math.Pow(r, n) * (r - 1) / (Math.Pow(r, n) - 1);
+    var iznosKamata = anuitet * n - iznos;
+    return (anuitet, iznosKamata);
+}
+
+double Kamatnjak(double stopa)
+{
+    return 1 + stopa / 100 / 12;
+}
+
+var nastavi = true;
+do
+{
+    Console.Write("Glavnica: ");
+    var unos = Console.ReadLine();
+    if (string.IsNullOrEmpty(unos))
+        nastavi = false;
+    else
+    {
+        var ok = double.TryParse(unos, out double glavnica);
+        if (!ok)
+        {
+            Console.WriteLine("Pogrešan format broja");
+            continue;
+        }
+        Console.Write("Stopa: ");
+        unos = Console.ReadLine();
+        ok = double.TryParse(unos, out double stopa);
+        if (!ok)
+        {
+            Console.WriteLine("Pogrešan format broja");
+            continue;
+        }
+        Console.Write("Broj godina: ");
+        unos = Console.ReadLine();
+        ok = int.TryParse(unos, out int brojGodina);
+        if (!ok)
+        {
+            Console.WriteLine("Pogrešan format broja");
+            continue;
+        }
+        try
+        {
+            var rezultat = IzracunParametara(glavnica, stopa, brojGodina);
+            Console.WriteLine("Za zadani iznos kredita {0:N2}, godišnju kamatnu stopu {1} i broj godina otplate {2} anuitet je {3:N2} a ukupno plaćena kamata {4:N2}", glavnica, stopa, brojGodina, rezultat.anuitet, rezultat.iznosKamata);
+        }
+        catch (Exception ex)
+        {
+            Console.WriteLine("Dogodila se pogreška. Tekst: {0}", ex.Message);
+        }
+
+    }
+} while (nastavi);
+
---
+++ Zarada/Program.cs
@@ -0,0 +1,48 @@
+﻿using System.Net.Http.Headers;
+
+double Zarada(int brojSati, double cijenaSata = 40.0, double porez = 0.2)
+{
+    if (brojSati < 0)
+        throw new ArgumentException("Broj sati mora biti > 0");
+    if (cijenaSata < 0)
+        throw new ArgumentException("Cijena sata mora biti > 0");
+    return brojSati * cijenaSata * (1 - porez);
+}
+
+var nastavi = true;
+do
+{
+    Console.Write("Broj sati: ");
+    var unos = Console.ReadLine();
+    if (string.IsNullOrEmpty(unos))
+        nastavi = false;
+    else
+    {
+        var ok = int.TryParse(unos, out int brojSati);
+        if (!ok)
+        {
+            Console.WriteLine("Pogrešan format broja");
+            continue;
+        }
+        Console.Write("Cijena sata: ");
+        unos = Console.ReadLine();
+        ok = double.TryParse(unos, out double cijenaSata);
+        if (!ok)
+        {
+            Console.WriteLine("Pogrešan format broja");
+            continue;
+        }
+        Console.Write("Porez: ");
+        unos = Console.ReadLine();
+        ok = double.TryParse(unos, out double porez);
+        if (!ok)
+        {
+            Console.WriteLine("Pogrešan format broja");
+            continue;
+        }
+        Console.WriteLine("Ukupna zarada za broj sati: {0}, cijenu sata {1} i porez {2} je {3}",brojSati, cijenaSata, porez, Zarada(brojSati, cijenaSata, porez));
+        Console.WriteLine("Ukupna zarada za broj sati {0}, cijenu sata {1} i defautnu stopu poreza je {2}", brojSati, cijenaSata, Zarada(brojSati, cijenaSata));
+        Console.WriteLine("Ukupna zarada za broj sati {0}, defaultnu cijenu sata i porez {1} je {2}", brojSati, porez, Zarada(brojSati, porez: porez));
+        Console.WriteLine("Ukupna zarada za broj sati {0} i defaultnu cijenu sata i stopu poreza je {1}", brojSati, Zarada(brojSati));
+    }
+} while (nastavi);
