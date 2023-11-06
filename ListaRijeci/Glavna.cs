d358417d8ef9b5d0d1dbb120f9a9153b9e49e67c
tvbegovic <tvrtko@adamant.hr>
2023-11-06 10:58:36 +0100

    liste, ponavljanje blic2

---
+++ ListaRijeci/Glavna.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ListaRijeci
+{
+    public partial class Glavna : Form
+    {
+        List<string> rijeci = new List<string>();
+        public Glavna()
+        {
+            InitializeComponent();
+        }
+
+        void AzurirajListBox()
+        {
+            lstRijeci.DataSource = null;
+            lstRijeci.DataSource = rijeci;
+        }
+
+        private void btnDodajNaKraj_Click(object sender, EventArgs e)
+        {
+            var rijec = txtJednaRijec.Text;
+            rijeci.Add(rijec);
+            AzurirajListBox();
+        }
+
+        private void btnDodajNaPoziciju_Click(object sender, EventArgs e)
+        {
+            var unos = txtPozicija.Text;
+            var ok = int.TryParse(unos, out int pozicija);
+            if(!ok)
+            {
+                MessageBox.Show("Broj je u pogrešnom formatu");
+                return;
+            }
+            if(pozicija < 0 || pozicija > rijeci.Count)
+            {
+                MessageBox.Show("Pozicija je izvan raspona");
+                return;
+            }
+            var rijec = txtJednaRijec.Text;
+            rijeci.Insert(pozicija, rijec);
+            AzurirajListBox();
+        }
+
+        private void btnDodajVise_Click(object sender, EventArgs e)
+        {
+            var poljeRijeci = txtViseRijeci.Text.Split(' ');
+            rijeci.AddRange(poljeRijeci);
+            AzurirajListBox();
+        }
+
+        private void btnUkloni_Click(object sender, EventArgs e)
+        {
+            var index = lstRijeci.SelectedIndex;
+            if(index >= 0)
+            {
+                rijeci.RemoveAt(index);
+                AzurirajListBox();
+            }
+        }
+
+        private void btnIsprazni_Click(object sender, EventArgs e)
+        {
+            rijeci.Clear();
+            AzurirajListBox();
+        }
+    }
+}
---
+++ VjezbaBlic2/Program.cs
@@ -0,0 +1,40 @@
+﻿void Izracun(int broj, out int sumaParnih, out int sumaSvih)
+{
+    if (broj < 1 || broj > 1000)
+        throw new ArgumentException("Broj je izvan raspona");
+    sumaParnih = 0;
+    sumaSvih = 0;
+    for (int i = 1; i <= broj; i++)
+    {
+        if (i % 2 == 0)
+            sumaParnih += i;
+        sumaSvih += i;
+    }
+}
+
+var nastavi = true;
+do
+{
+    Console.Write("Unesite broj: ");
+    var unos = Console.ReadLine();
+    if (string.IsNullOrEmpty(unos))
+        nastavi = false;
+    else
+    {
+        var ok = int.TryParse(unos, out int broj);
+        if (!ok)
+        {
+            Console.WriteLine("Pogrešan format broja");
+            continue;
+        }
+        try
+        {
+            Izracun(broj, out int sumaParnih, out int sumaSve);
+            Console.WriteLine("Za uneseni broj {0} suma parnih brojeva manjih od njega je {1} a suma svih je {2}", broj, sumaParnih, sumaSve);
+        }
+        catch (Exception ex)
+        {
+            Console.WriteLine("Dogodila se pogreška. Tekst: {0}", ex.Message);
+        }
+    }
+} while (nastavi);
