c4be6b2c6dbdf5ebecb5aeefd6a45dac23cac0b1
tvbegovic <tvrtko@adamant.hr>
2023-11-08 10:38:41 +0100

    klase

---
+++ Igre_Klase/Glavna.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Igre_Klase
+{
+    public partial class Glavna : Form
+    {
+        public Glavna()
+        {
+            InitializeComponent();
+        }
+
+        private void btnUnesi_Click(object sender, EventArgs e)
+        {
+            var naziv = txtNaziv.Text;
+            var opis = txtOpis.Text;
+            var vrsta = txtVrsta.Text;
+            var izdavac = txtIzdavac.Text;
+            var unos = txtDatum.Text;
+            var ok = DateTime.TryParse(unos, out DateTime datum);
+            if(!ok)
+            {
+                MessageBox.Show("Pogrešan format datuma");
+                return;
+            }
+            unos = txtCijena.Text;
+            ok = double.TryParse(unos, out double cijena);
+            if (!ok)
+            {
+                MessageBox.Show("Pogrešan format broja");
+                return;
+            }
+            var igra = new Igra();
+            try
+            {
+                igra.UnesiPodatke(naziv, opis, vrsta, izdavac, datum, cijena);
+                MessageBox.Show("Podaci su uspješno uneseni");
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show($"Dogodila se pogreška. Tekst: {ex.Message}");
+            }
+        }
+    }
+}
---
+++ Igre_Klase/Igra.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Igre_Klase
+{
+    internal class Igra
+    {
+        private string naziv, opis, vrsta, izdavac;
+        private DateTime datumIzdavanja;
+        private double cijena;
+
+        public void UnesiPodatke(string naziv, string opis, string vrsta, string izdavac, DateTime datumIzdavanja, double cijena)
+        {
+            if (string.IsNullOrEmpty(naziv))
+                throw new ArgumentException("Naziv mora biti upisan");
+
+            this.naziv = naziv;
+            this.opis = opis;
+            this.vrsta = vrsta;
+            this.izdavac = izdavac;
+            this.datumIzdavanja = datumIzdavanja;
+            this.cijena = cijena;
+        }
+    }
+}
